6ad4091e0f535471c6b91109884fea843f3f523a
Emilysworld-Testing <em.smulders24@gmail.com>
2025-05-23 19:40:05 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,11 @@ dotenv.config();
 // EDIT THIS!
 function postTextFromImageName(imageName: string): string {
   // Remove the file extension and parse the date
-  const dateParts = imageName.replace('.jpg', '').split('-');
+  const dateParts = imageName.replace('', '').split('-');
   const date = new Date(Number(dateParts[0]), Number(dateParts[1]) - 1, Number(dateParts[2] || 1));
 
   // Create a formatter
-  const formatter = new Intl.DateTimeFormat('en-US', {
+  const formatter = new Intl.DateTimeFormat('en-UK', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
